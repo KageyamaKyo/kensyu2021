53d182b87561340ca16380c99450cc5ad42af4fc
KageyamaKyo <kageyama-kyo@webrage.jp>
2021-08-05 14:21:56 +0900

    [update]テーブルタグを追加いたしました。
    テーブルタグが抜けていたため。

--- 03/login02.php
+++ 03/login02.php
@@ -7,12 +7,14 @@
     </head>
     <body>
         <form method='post' action='result02.php'>
-        <tr>
-            <td><input type="text" name="id" size="20" maxlength="28" placeholder="ID" /></td>
-            <td><input type="password" name="password" size="20" maxlength="28" placeholder="PASSWORD" /></td>
-        </tr>
-        <p><input type="submit" value="ログイン">
-           <input type="reset" value="リセット"></p>
+            <table>
+                <tr>
+                    <td><input type="text" name="id" size="20" maxlength="28" placeholder="ID" /></td>
+                    <td><input type="password" name="password" size="20" maxlength="28" placeholder="PASSWORD" /></td>
+                </tr>
+                <p><input type="submit" value="ログイン">
+                    <input type="reset" value="リセット"></p>
+            </table>
         </form>
     </body>
 </html>
