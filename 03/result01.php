c0771ef61cbe4d73e171c593144ad6fc3a0a2ad5
KageyamaKyo <kageyama-kyo@webrage.jp>
2021-08-05 15:05:50 +0900

    [update]htmlテンプレートの追加と文言の修正
    Because the words were inadequate.

--- 03/result01.php
+++ 03/result01.php
@@ -1,10 +1,20 @@
-<?php
-    $id = "kageyama";
-    $password = "0123";
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset='utf-8'>
+        <meta name='viewport' content='width=device-width, initial-scale=1'>
+        <title>ログイン画面</title>
+    </head>
+    <body>
+        <?php
+            $id = "kageyama";
+            $password = "0123";
 
-    if($_POST['id'] == $id AND $_POST['password'] == $password){
-        echo 'ログイン成功おめでとう';
-    } else {
-            echo 'ログイン失敗してるで';
-    }
-?>
+            if($_POST['id'] == $id AND $_POST['password'] == $password){
+                echo 'ログイン成功';
+            } else {
+                echo 'ログイン失敗';
+            }
+        ?>
+    </body>
+</html>
--- 03/result02.php
+++ 03/result02.php
@@ -1,14 +1,24 @@
-<?php
-    $gold_id = "01";
-    $gold_pw = "gold";
-    $reg_id = "02";
-    $reg_pw = "reg";
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset='utf-8'>
+        <meta name='viewport' content='width=device-width, initial-scale=1'>
+        <title>ログイン画面</title>
+    </head>
+    <body>
+        <?php
+            $gold_id = "01";
+            $gold_pw = "gold";
+            $reg_id = "02";
+            $reg_pw = "reg";
 
-    if($_POST['id'] == $gold_id AND $_POST['password'] == $gold_pw){
-    echo 'ゴールド会員やんけ！';
-    }elseif($_POST['id'] == $reg_id AND $_POST['password'] == $reg_pw){
-    echo 'まだレギュラー会員？';
-    }else{
-    echo 'ログイン失敗してるで！';
-    }
-?>
+            if($_POST['id'] == $gold_id AND $_POST['password'] == $gold_pw){
+                echo 'ゴールド会員：ログイン成功';
+            }elseif($_POST['id'] == $reg_id AND $_POST['password'] == $reg_pw){
+                echo 'レギュラー会員：ログイン成功';
+            }else{
+                echo 'ログイン失敗';
+            }
+        ?>
+    </body>
+</html>
