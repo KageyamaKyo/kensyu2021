8052939aa6c8f4822cffe33a2f7acb534e8acf1d
KageyamaKyo <kageyama-kyo@webrage.jp>
2021-08-05 14:23:29 +0900

    [add]行数指定したテーブルを出力するファイルを追加いたしました。
    課題報告のため。

---
+++ 03/loop01.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset='utf-8'>
+        <meta name='viewport' content='width=device-width, initial-scale=1'>
+        <title>指定行数テーブル</title>
+    </head>
+    <body>
+        <form method='post' action='loop01.php'>
+        <table>
+            <tr>
+                <td><input type="number" name="cols" min="1" max="6" />行のテーブルを作成する</td>
+            </tr>
+        </table>
+        <p><input type="submit" value="送信">
+           <input type="reset" value="取り消し"></p>
+        </form>
+        <table border="1" style="border-collapse:collapse;">
+            <?php
+                for($i=0; $i < $_POST['cols']; $i++){
+                    echo "<tr><td>水</td><td>火</td><td>自然</td><td>闇</td><td>光</td></tr>";
+                }
+            ?>
+        </table>
+    </body>
+</html>
