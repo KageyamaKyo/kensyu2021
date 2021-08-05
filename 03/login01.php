0b61f224e3fe00dde25877a22dbbe52cd2986193
KageyamaKyo <kageyama-kyo@webrage.jp>
2021-08-05 14:59:49 +0900

    [update]テーブルタグの削除
    送信ボタンと取り消しボタンの位置調整のため。

--- 03/login01.php
+++ 03/login01.php
@@ -7,14 +7,10 @@
     </head>
     <body>
         <form method='post' action='result01.php'>
-            <table>
-                <tr>
-                    <td><input type="text" name="id" size="20" maxlength="28" placeholder="ID" /></td>
-                    <td><input type="password" name="password" size="20" maxlength="28" placeholder="PASSWORD" /></td>
-                </tr>
-                <p><input type="submit" value="ログイン">
-                    <input type="reset" value="リセット"></p>
-            </table>
+            <p><input type="text" name="id" size="20" maxlength="28" placeholder="ID" />
+                <input type="password" name="password" size="20" maxlength="28" placeholder="PASSWORD" /></p>
+            <p><input type="submit" value="ログイン">
+                <input type="reset" value="リセット"></p>
         </form>
     </body>
 </html>
