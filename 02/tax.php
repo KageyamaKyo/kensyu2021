2e344746926ee7507b6368319852da32a11427b5
KageyamaKyo <kageyama-kyo@webrage.jp>
2021-08-06 11:47:34 +0900

    [update]echoの追加
    echoが抜けていることにより、<tr>タグが閉じれていない現象を解消しました。

--- 02/tax.php
+++ 02/tax.php
@@ -140,7 +140,8 @@
                 <td colspan="4" style="text-align:left;">合計</td>
                 <td>
                     <?php
-                        echo $price = $price_1 + $price_2 + $price_3 + $price_4 + $price_5 . "円（税込み）";
+                        $price = $price_1 + $price_2 + $price_3 + $price_4 + $price_5;
+                        echo $price . "円（税込み）";
                     ?>
                 </td>
             </tr>
